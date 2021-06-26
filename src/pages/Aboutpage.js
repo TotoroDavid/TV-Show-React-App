71dcbc55e5a1dd6ff4e23276f25b17c2455b4adf
TotoroDavid <davidtoro65@hotmail.com>
2021-06-26 11:59:33 -0500

    starting the project

--- src/pages/Aboutpage.js
+++
@@ -1,9 +0,0 @@
-const Aboutpage = () => {
-    return (
-        <div className="aboutpage">
-            this is the about page
-        </div>
-    )
-}
-
-export default Aboutpage
--- src/pages/Singlepage.js
+++
@@ -1,9 +0,0 @@
-const Singlepage = () => {
-    return (
-        <div className="singlepage">
-            this is a single page
-        </div>
-    )
-}
-
-export default Singlepage
